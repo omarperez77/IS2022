ba40af593382c0d94784c6330bb76fefbd77c96c
Omar Pérez <omarg.perezm@gmail.com>
2022-04-06 13:22:26 -0500

    Actualización del menu del sistema

---
+++ sce-tecmm2022/src/pages/Ayuda.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page>
+    <h1>Ayuda</h1>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
--- sce-tecmm2022/src/router/routes.js
+++ sce-tecmm2022/src/router/routes.js
@@ -4,7 +4,14 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/help', component: () => import('pages/Ayuda.vue') },
+      { path: '/profesores', component: () => import('pages/profesores/index.vue') },
+      { path: '/newprofesor', component: () => import('pages/profesores/new.vue') },
+      { path: '/editprofesor', component: () => import('pages/profesores/edit.vue') },
+      { path: '/estudiantes', component: () => import('pages/estudiantes/index.vue') },
+      { path: '/newestudiante', component: () => import('pages/estudiantes/new.vue') },
+      { path: '/editestudiante', component: () => import('pages/estudiantes/edit.vue') }
     ]
   },
 
